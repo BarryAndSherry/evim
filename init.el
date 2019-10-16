98ccdbcc557e3c56024fd700a6bf0fe9e8c9b396
barry <barry_w@foxmail.com>
2019-10-16 19:42:33 +0800

    changed theme to one dark

--- init.el
+++ init.el
@@ -67,32 +67,3 @@
 (require 'init-latex)
 (require 'init-evil)
 (require 'init-ui)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-
-'(TeX-source-correlate-method (quote synctex))
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(TeX-view-program-list (quote (("Okular" "okular --unique %o#src:%n%b"))))
- '(TeX-view-program-selection (quote ((engine-omega "dvips and gv") (output-dvi "xdvi") (output-pdf "Okular") (output-html "xdg-open"))))
- 
- '(doom-themes-enable-bold t)
- '(doom-themes-enable-italic t)
- '(imenu-list-auto-resize t)
- '(imenu-list-focus-after-activation t)
- '(org-agenda-files (quote ("~/external/notes/inbox/inbox.org")))
- '(package-selected-packages
-   (quote
-    (cdlatex auctex ob-ipython ob-sagemath darkroom yasnippet winum which-key vdiff use-package smartparens ripgrep rainbow-mode rainbow-delimiters projectile org-bullets neotree minimap key-chord imenu-list highlight-indent-guides evil doom-themes doom-modeline counsel company-ycmd company-irony avy autothemer)))
- '(yas-snippet-dirs (quote ("~/.emacs.d/snippets"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(imenu-list-entry-face-1 ((t (:foreground "white"))))
- '(vertical-bar (doom-darken base5 0.4)))
