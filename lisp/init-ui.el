8cb6f2bd1742a43a64ab39e25eea54f80831fdbf
barry <barry_w@foxmail.com>
2019-10-27 11:53:47 +0800

    fix font size

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -4,12 +4,12 @@
 ;; (setq default-frame-alist '( (fullscreen . maximized)))
 
 
+;; TODO : I could not fix this  for setting font size acordding to the screen reslution
+;; (setq default-frame-alist '((font . "ShureTechMono Nerd Font 12")))
+
 (if (< (display-pixel-width) 1921) 
     (set-default-font "ShureTechMono Nerd Font 12")
   (set-default-font "ShureTechMono Nerd Font 14"))
-
-;; TODO : I could not fix this  for setting font size acordding to the screen reslution
-(setq default-frame-alist '((font . "ShureTechMono Nerd Font 12")))
 
 
 
